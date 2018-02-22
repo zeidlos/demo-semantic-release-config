073235d2d87f450198d9d25130fb399279fc024a
Julius Zeidler <julius.zeidler@starfinanz.de>
2018-02-22 15:53:25 +0100

    feat: Initial Release for config

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "release": {
+    "verifyConditions": [
+      "@semantic-release/changelog",
+      "@semantic-release/git",
+      "@semantic-release/github"
+    ],
+    "prepare": [
+      "@semantic-release/changelog",
+      "@semantic-release/git"
+    ],
+    "publish": [
+      {
+        "path": "@semantic-release/git",
+        "assets": [
+          "package.json",
+          "npm-shrinkwrap.json"
+        ]
+      },
+      {
+        "path": "@semantic-release/github"
+      }
+    ]
+  }
+};
