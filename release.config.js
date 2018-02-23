94b2606fa26fcddc5dfe05bde1cc3c9da112e094
Julius Zeidler <julius.zeidler@starfinanz.de>
2018-02-23 14:41:37 +0100

    fix(config): Hopefully fixes config issues

--- release.config.js
+++ release.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-  "release": {
-    "verifyConditions": [
-      "@semantic-release/changelog",
-      "@semantic-release/git",
-      "@semantic-release/github"
-    ],
-    "prepare": [
-      "@semantic-release/changelog",
-      "@semantic-release/git"
-    ],
-    "publish": [
-      {
-        "path": "@semantic-release/git",
-        "assets": [
-          "package.json",
-          "npm-shrinkwrap.json"
-        ]
-      },
-      {
-        "path": "@semantic-release/github"
-      }
-    ]
-  }
+  "verifyConditions": [
+    "@semantic-release/changelog",
+    "@semantic-release/git",
+    "@semantic-release/github"
+  ],
+  "prepare": [
+    "@semantic-release/changelog",
+    "@semantic-release/git"
+  ],
+  "publish": [
+    {
+      "path": "@semantic-release/git",
+      "assets": [
+        "package.json",
+        "npm-shrinkwrap.json"
+      ]
+    },
+    {
+      "path": "@semantic-release/github"
+    }
+  ]
 };
